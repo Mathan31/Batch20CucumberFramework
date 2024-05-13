cdbe909ce2d95a3a84c3f6ebde83c5a1516d9a37
Mathan C <mathan.chandrasekar@gmail.com>
2024-05-13 09:48:38 +0530

    Parallel Implemented

--- src/test/java/runner/TestNGRunner.java
+++ src/test/java/runner/TestNGRunner.java
@@ -1,5 +1,7 @@
 package runner;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -14,5 +16,11 @@ import io.cucumber.testng.CucumberOptions;
 		publish = true
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests{
+	
+	@DataProvider(parallel = true)
+    public Object[][] scenarios() {
+       
+        return super.scenarios();
+    }
 
 }
